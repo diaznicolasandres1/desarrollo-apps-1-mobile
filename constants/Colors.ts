aaa3ea7bdbc7722b31c0f92f6c602e91bd06ec80
Tomas Schuster <ts2525@hotmail.com>
2025-05-15 21:13:45 -0300

    add colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,57 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    text: "#11181C",
+    background: "#FFF3E0", // Orange 50
+    tint: "#FF9800", // Orange 500
+    icon: "#687076",
+    tabIconDefault: "#687076",
+    tabIconSelected: "#FF9800", // Orange 500
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    text: "#ECEDEE",
+    background: "#495B43", // Olive 800
+    tint: "#E65100", // Orange 900
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
+    tabIconSelected: "#E65100", // Orange 900
+  },
+  primary: {
+    orange50: "#FFF3E0",
+    orange100: "#FFE0B2",
+    orange200: "#FFE0B2",
+    orange300: "#FFB74D",
+    orange400: "#FFA726",
+    orange500: "#FF9800",
+    orange600: "#FB8C00",
+    orange700: "#F57C00",
+    orange800: "#EF6C00",
+    orange900: "#E65100",
+  },
+  secondary: {
+    olive50: "#F5F8F4",
+    olive100: "#EDF3EC",
+    olive200: "#D9E6D8",
+    olive300: "#C5D9C4",
+    olive400: "#A6C29F",
+    olive500: "#8BAC81",
+    olive600: "#6D8764",
+    olive700: "#5C7354",
+    olive800: "#495B43",
+    olive900: "#364433",
+  },
+  azul: {
+    azul50: "#E3F2FD",
+    azul100: "#BBDEFB",
+    azul200: "#90CAF9",
+    azul300: "#64B5F6",
+    azul400: "#42A5F5",
+    azul500: "#2196F3",
+    azul600: "#1E88E5",
+    azul700: "#1976D2",
+    azul800: "#1565C0",
+    azul900: "#0D47A1",
   },
 };
