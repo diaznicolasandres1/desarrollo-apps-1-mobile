40063ba6349b1e62683c76216aae054c0cea443c
ges <gonzalosalvia@gmail.com>
2025-06-28 20:22:36 -0300

    correction de config.ts

--- constants/config.ts
+++ constants/config.ts
@@ -1,2 +1,2 @@
-//export const BASE_URL = "https://desarrollo-apps-1-back-end.vercel.app";
-export const BASE_URL = "http://192.168.50.116:3000";
+export const BASE_URL = "https://desarrollo-apps-1-back-end.vercel.app";
+//export const BASE_URL = "http://192.168.50.116:3000";
